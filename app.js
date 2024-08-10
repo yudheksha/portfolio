52978cb5e42efe89f2f9e382c50c3088e1dd524e
yudheksha <yudithaagk@gmail.com>
2024-08-09 20:06:14 -0400

    Updated

--- app.js
+++ app.js
@@ -1,63 +1,74 @@
-let sections = document.querySelectorAll('section')
-let navLinks = document.querySelectorAll('header nav a')
-let menuIcon = document.querySelector('#menu-icon')
-let navbar = document.querySelector('.navbar')
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x')
-    navbar.classList.toggle('active')
-}
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
 
-document.addEventListener('DOMContentLoaded', function() {
-    const contactButton = document.querySelector('.btn-box a[href^="mailto:"]');
-    contactButton.addEventListener('click', function(event) {
-        // You can set subject and body dynamically if needed
-        const subject = encodeURIComponent("Exciting Opportunity! We'd like to offer you the position of ...");
-        const body = encodeURIComponent("Dear Yudheksha,\n\nWe are pleased to extend the following offer to you...");
-        this.href = `mailto:yugokavi@iu.edu?subject=${subject}&body=${body}`;
-        
-        // Log or handle the click event as needed
-        console.log("Contact Me button was clicked.");
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  const contactButton = document.querySelector('.btn-box a[href^="mailto:"]');
+  contactButton.addEventListener("click", function (event) {
+    // You can set subject and body dynamically if needed
+    const subject = encodeURIComponent(
+      "Exciting Opportunity! We'd like to offer you the position of ..."
+    );
+    const body = encodeURIComponent(
+      "Dear Yudheksha,\n\nWe are pleased to extend the following offer to you..."
+    );
+    this.href = `mailto:yugokavi@iu.edu?subject=${subject}&body=${body}`;
+
+    // Log or handle the click event as needed
+    console.log("Contact Me button was clicked.");
+  });
 });
-
 
 window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY
-        let offset = sec.offsetTop - 100
-        let height = sec.offsetHeight
-        let id = sec.getAttribute('id')
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
-        if(top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active')
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
-            })
-        }
-    })
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
 
-    menuIcon.classList.remove('bx-x')
-    navbar.classList.remove('active')
-}
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
 
 ScrollReveal({
-    reset: true,
-    distance: '15px',
-    duration: 2000,
-    delay: 200
+  reset: true,
+  distance: "15px",
+  duration: 2000,
+  delay: 200,
 });
 
-ScrollReveal().reveal('.left', {origin: 'left'})
-ScrollReveal().reveal('.right', {origin: 'right'})
-ScrollReveal().reveal('.top', {origin: 'top'})
-ScrollReveal().reveal('.bottom', {origin: 'bottom'})
+ScrollReveal().reveal(".left", { origin: "left" });
+ScrollReveal().reveal(".right", { origin: "right" });
+ScrollReveal().reveal(".top", { origin: "top" });
+ScrollReveal().reveal(".bottom", { origin: "bottom" });
 
+const typed = new Typed(".multiple-text", {
+  strings: [
+    "Coder",
+    "Software Developer",
+    "Full Stack Developer",
+    "Data Analyst",
+    "Machine Learning Engineer",
+  ],
 
-const typed = new Typed('.multiple-text', {
-    strings: ['Software Developer', 'Full Stack Developer', 'Coder', 'Machine Learning Engineer', 'Data Analyst'],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-})
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
